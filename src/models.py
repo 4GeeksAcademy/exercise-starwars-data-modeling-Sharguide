1a81ef8ce04b39a21dbcd30147873aacd38fa7d0
Guillermo Obando <guillermoj.obando@gmail.com>
2024-09-11 20:19:53 +0000

    Adding character and planet table

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -19,16 +19,25 @@ class User(Base):
     date_of_subscription = Column(Date, index=True)
 
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    gravity = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=False)
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(Float, nullable=False)
+    eye_color = Column(String(250))
+    mass = Column(Integer)
 
     def to_dict(self):
         return {}
