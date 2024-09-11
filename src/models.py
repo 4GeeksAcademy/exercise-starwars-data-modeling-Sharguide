40ca7bf688651173fede0fd9eb90ca799ceb9869
Guillermo Obando <guillermoj.obando@gmail.com>
2024-09-11 19:01:03 +0000

    User table update

--- src/models.py
+++ src/models.py
@@ -7,12 +7,17 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    date_of_subscription = Column(Date, index=True)
+
 
 class Address(Base):
     __tablename__ = 'address'
